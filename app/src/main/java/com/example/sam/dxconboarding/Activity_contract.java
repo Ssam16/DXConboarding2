a04b5508ee00d1ae220379e705ac4b6f3f3d740b
Ssam16 <samsukllikar@gmail.com>
2018-11-28 10:10:23 +0000

    First try

---
+++ app/src/main/java/com/example/sam/dxconboarding/Activity_contract.java
@@ -0,0 +1,13 @@
+package com.example.sam.dxconboarding;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_contract extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contract);
+    }
+}
